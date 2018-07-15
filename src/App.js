9d4fba1d8167cf677db38bd1f3e2e738b1859156
Pavel Novitskyi <novipsh@outlook.com>
2018-07-15 21:12:25 +0300

    Initialize React app with create-react-app
    Commited before run npm run eject

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import { MDCRipple } from '@material/ripple';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>TMDb API REACT APP</p>
+      </div>
+    );
+  }
+}
+
+export default App;
